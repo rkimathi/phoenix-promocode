671ac9f3bf05f4b43649a45cc4069e81b29ecb3d
Rodney Kimathi <rodneykimathi@gmail.com>
2019-01-19 17:21:38 +0300

    Change Code schema

--- lib/brave/promo/code.ex
+++ lib/brave/promo/code.ex
@@ -5,6 +5,11 @@ defmodule Brave.Promo.Code do
 
   schema "codes" do
     field :code, :string
+    field :amount, :integer
+    field :expiry_date, :date
+    field :is_active, :boolean, default: true
+    field :event_location, {:array, :float}
+    field :radius, :integer
 
     timestamps()
   end
@@ -12,8 +17,8 @@ defmodule Brave.Promo.Code do
   @doc false
   def changeset(code, attrs) do
     code
-    |> cast(attrs, [:code])
-    |> validate_required([:code])
+    |> cast(attrs, [:code, :amount, :expiry_date, :is_active, :event_location, :radius])
+    |> validate_required([:code, :amount, :expiry_date, :is_active, :event_location, :radius])
     |> unique_constraint(:code)
   end
 end
