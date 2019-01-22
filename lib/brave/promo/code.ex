7dfd52f717aab81129ff37d18210c8bde7863bcf
Rodney Kimathi <rodneykimathi@gmail.com>
2019-01-22 05:17:33 +0300

    Change validation rules

--- lib/brave/promo/code.ex
+++ lib/brave/promo/code.ex
@@ -7,7 +7,7 @@ defmodule Brave.Promo.Code do
     field :code, :string
     field :amount, :integer
     field :expiry_date, :date
-    field :is_active, :boolean, default: true
+    field :is_active, :boolean, default: false
     field :event_location, {:array, :float}
     field :radius, :integer
 
@@ -18,7 +18,7 @@ defmodule Brave.Promo.Code do
   def changeset(code, attrs) do
     code
     |> cast(attrs, [:code, :amount, :expiry_date, :is_active, :event_location, :radius])
-    |> validate_required([:code, :amount, :expiry_date, :is_active, :event_location, :radius])
+    |> validate_required([:code, :amount, :expiry_date, :event_location, :radius])
     |> unique_constraint(:code)
   end
 end
