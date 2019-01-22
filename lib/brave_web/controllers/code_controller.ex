5989504b1ef26a56654d530e96e49894e4681148
Rodney Kimathi <rodneykimathi@gmail.com>
2019-01-22 05:18:10 +0300

    Add new logic

--- lib/brave_web/controllers/code_controller.ex
+++ lib/brave_web/controllers/code_controller.ex
@@ -11,8 +11,15 @@ defmodule BraveWeb.CodeController do
     render(conn, "index.json", codes: codes)
   end
 
-  def create(conn, %{"code" => code_params}) do
-    with {:ok, %Code{} = code} <- Promo.create_code(code_params) do
+  def create(conn, params) do
+    {amount, _} = Integer.parse(params["amount"])
+    {_, expiry_date} = Date.from_iso8601(params["expiry_date"])
+    event_location = Jason.decode!(params["event_location"])
+    {radius, _} = Integer.parse(params["radius"])
+    params = %{params | "amount" => amount, "expiry_date" => expiry_date, 
+        "event_location" => event_location, "radius" => radius}
+    
+    with {:ok, %Code{} = code} <- Promo.create_code(params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.code_path(conn, :show, code))
@@ -25,10 +32,16 @@ defmodule BraveWeb.CodeController do
     render(conn, "show.json", code: code)
   end
 
-  def update(conn, %{"id" => id, "code" => code_params}) do
-    code = Promo.get_code!(id)
+  def update(conn, params) do
+    {amount, _} = Integer.parse(params["amount"])
+    {_, expiry_date} = Date.from_iso8601(params["expiry_date"])
+    event_location = Jason.decode!(params["event_location"])
+    {radius, _} = Integer.parse(params["radius"])
+    params = %{params | "amount" => amount, "expiry_date" => expiry_date, 
+        "event_location" => event_location, "radius" => radius}
+    code = Promo.get_code!(params["id"])
 
-    with {:ok, %Code{} = code} <- Promo.update_code(code, code_params) do
+    with {:ok, %Code{} = code} <- Promo.update_code(code, params) do
       render(conn, "show.json", code: code)
     end
   end
@@ -38,6 +51,35 @@ defmodule BraveWeb.CodeController do
 
     with {:ok, %Code{}} <- Promo.delete_code(code) do
       send_resp(conn, :no_content, "")
+    end
+  end
+
+  def active(conn, _params) do
+    codes = Promo.list_active_codes()
+    render(conn, "index.json", codes: codes)
+  end
+
+  def promo(conn, params) do
+    response = valid(params)
+    
+    with {:ok, code} <- response do
+      render(conn, "show.json", code: code)
+    # else
+    #   {:error, message} -> message
+    end
+  end
+
+  defp valid(params) do
+    code = Promo.get_code_by_code!(params["code"])
+    origin = Jason.decode!(params["origin"])
+    destination = Jason.decode!(params["destination"])
+    origin_valid? = Geocalc.within?(100000, code.event_location, origin)
+    destination_valid? = Geocalc.within?(100000, code.event_location, destination)
+    
+    cond do
+      origin_valid? == false -> {:error, "Origin is invalid"}
+      destination_valid? == false -> {:error, "Destination is invalid"}
+      origin_valid? && destination_valid? == true -> {:ok, code}
     end
   end
 end
